0ba32b533bc28dafe6b7fc0b7e97ea7a693f9056
maxximus87 <maxxturner10@gmail.com>
2016-11-01 16:57:22 -0400

    new winning strat

--- unbeatable.rb
+++ unbeatable.rb
@@ -7,5 +7,13 @@ class UnbeatableAi
 
 	def get_move(board)
 		board.index("")
+		super('o', board)
 	end
+
+	def winning_move(player)
+		
 end
+
+
+ WINNING_SPACES = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+ WINNING_PRIORITIES = [[0, 1, 2], [0, 2, 1], [1, 2, 0]]
--- unbeatabletest.rb
+++ unbeatabletest.rb
@@ -13,4 +13,14 @@ class TestUnbeatable <Minitest::Test
 		assert_equal(true, [0, 1, 2, 3, 4, 5, 6, 7, 8].include?(player.get_move(["", "", "", "", "", "", "", "", ""])))
 	end
 
+	def test_for_win_at_zero
+		player = UnbeatableAi.new("x")
+		assert_equal(0, player.get_move(["", "x", "x", "", "", "", "", "", ""]))
+	end
+
+	def test_for_win_at_five
+		player = UnbeatableAi.new("x")
+		assert_equal(5, player.get_move(["", "", "", "x", "x", "", "", "", ""]))
+	end
+
 end
